73d61901f7f4d18eed52234526d40be3c5e24159
manoj-axiomengg <developer@axiomengg.com>
2025-09-25 12:57:53 +0530

    Assignment 2

---
+++ Assignment 2/Task 1.py
@@ -0,0 +1,5 @@
+number = int(input("Enter A Number:"))
+if number % 2 == 0:
+    print("{} is an even number.".format(number))
+else:
+    print("{} is an odoo number.".format(number))    
---
+++ Assignment 2/Task 2.py
@@ -0,0 +1,4 @@
+total = 0
+for i in range(1, 51):
+    total = total + i
+print("The Sum of Number From 1 To 50 is : {}".format(total))    
